807ba8df76a5bf0193bf7a7aa47a97a16c978d46
Daniela Diaz Graziani <danieladiazgraziani12@gmail.com>
2022-08-13 08:16:28 -0300

    add config initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import axios from "axios";
 
 function App() {
+  // const url = "https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=d1a554ccfa4ee8b4ffc4283e062ef27a"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
